9ee9293bb36f7540d054dcca650a1e0bd0b96d25
Sean Shadle <seandshadle@gmail.com>
2020-09-30 17:54:11 -0500

    Completed MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import {useLocalStorage} from "./useLocalStorage"
+import useMedia from "./useMedia"
+
+export default function useDarkMode() {
+    const [enabledState, setEnabledState] = useLocalStorage('dark-mode-enabled');
+    const prefersDarkMode = usePrefersDarkMode();
+
+    const enabled =
+      typeof enabledState !== 'undefined' ? enabledState : prefersDarkMode;
+  
+    useEffect(
+      () => {
+        const className = 'dark-mode';
+        const element = window.document.body;
+        if (enabled) {
+          element.classList.add(className);
+        } else {
+          element.classList.remove(className);
+        }
+      },
+      [enabled] 
+    );
+  
+    
+    return [enabled, setEnabledState];
+  }
+  
+  function usePrefersDarkMode() {
+    return useMedia(['(prefers-color-scheme: dark)'], [true], false);
+  }
