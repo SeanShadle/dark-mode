131ae722727c4bed3809a38b49a26a9b1d231191
Sean Shadle <seandshadle@gmail.com>
2020-09-30 12:31:24 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
-
+ 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
